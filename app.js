b95ffb08dae12615ee36073a04f48bceb25db9c9
praveen krishna <praveenkrishna7665@gmail.com>
2024-05-06 23:07:40 +0530

    routes

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const homeRouter = require('./lib/modules/home/home');
+const loginRouter = require('./lib/modules/login/login');
+const profileRouter = require('./lib/modules/profile/profile');
+const taskMangeRouter = require('./lib/modules/task_mangement/task_management');
+const teamColabRouter = require('./lib/modules/team_colab/team_colab');
 
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
+app.use('/api',homeRouter);
+app.use('/api',loginRouter);
+app.use('/api',profileRouter);
+app.use('/api',taskMangeRouter);
+app.use('/api',teamColabRouter);
 
-// Sample data of tasks (for demonstration)
-const tasks = [
-    { id: 1, assignTo: "John Doe", assignFrom: "Alice Smith", title: "Task 1", description: "Description for Task 1", comments: ["Comment 1", "Comment 2"], status: "In Progress" },
-    { id: 2, assignTo: "Alice Smith", assignFrom: "John Doe", title: "Task 2", description: "Description for Task 2", comments: ["Comment 3"], status: "Completed" }
-];
 
-// Route to handle retrieving tasks assigned to a specific user
-app.get('/myTasks/:userId', (req, res) => {
-    const { userId } = req.params;
-
-    // Filter tasks assigned to the specific user
-    const userTasks = tasks.filter(task => task.assignTo === userId);
-
-    // Return tasks assigned to the user
-    res.status(200).json(userTasks);
-});
-
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
